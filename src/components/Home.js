72a284f1048a39e5993e09f20ff43659efb520da
Mariela <yordanova.mariela@gmail.com>
2022-05-21 15:11:39 -0400

    useeffect

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
+
 const Home = (props)=>{
+  const [apiResponse, setApiResponse]=useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`https://api.punkapi.com/v2/beers/random`)
+      setApiResponse(response.data)
+    }
+    fetchData()
+  })
 
   return(
       <div clasName='home'>
+        <ul className='beerList'>
+          {
+            Object.keys(apiResponse).length > 0 ?
+            apiResponse.results.map((beer, index) => {
+              return (
+                <li key= {index} className = 'beer-name'>
+                  {beer.name}
+                  <img className='Beer' alt="Beer"  src='#' onClick={() =>{
+                    props.fetch(beer.url)
+                  }}/>
+
+                </li>
+              )
+            })
+            :null
+          }
+      </ul>
         <button>Select</button>
       </div>
-  )
-} 
+          )
+        }
 
 export default Home
